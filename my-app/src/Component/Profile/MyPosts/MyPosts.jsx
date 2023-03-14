1383f5d3dd804c6fd6557412da9d1982c7e878d2
Sergei <sergei.avramenko199@yandex.ru>
2023-03-14 12:39:20 +0300

    87

--- my-app/src/Component/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/Component/Profile/MyPosts/MyPosts.jsx
@@ -1,30 +1,22 @@
 import React from "react";
 import p from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../Redux/profile-reducer";
 
 
 
-const MyPosts = (props) => {
 
-
+const MyPosts = React.memo ((props) => {
+    console.log("memo")
     let PostsElement = props.posts.map( ex => <Post message={ex.name} likesCount={ex.likesCount} /> )
-
     let newPostElement = React.createRef();
     let Click = () => {
-
         props.click();
-
-
     }
     let onPostChange = (e) => {
         let text = newPostElement.current.value;
-
         props.updateNewPostText(text);
     }
-
     return (
-
         <div className={p.postsBlock}>
             <h3>My post </h3>
             <div>
@@ -34,19 +26,11 @@ const MyPosts = (props) => {
                 <div>
                     <button onClick={Click }>Add post</button>
                 </div>
-
-
             </div>
             <div className={p.post}>
                 {PostsElement}
-
-
-
-
             </div>
         </div>
-
-
     );
-}
+})
 export default MyPosts;
