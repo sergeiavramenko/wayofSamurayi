6c63c608c59c138998d771b2d3bdbf5c01cdea9e
Sergei <sergei.avramenko199@yandex.ru>
2023-03-15 23:20:58 +0300

    94Supreme Lazy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.css';  
 import NavBar from './Component/NavBar/NavBar';
-import ProfileContainer from './Component/Profile/ProfileContainer';
-import Dialogs from "./Component/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import React from "react";
+import React, {Suspense} from "react";
 import News from "./Component/News/News";
-
 import Settings from "./Component/Settings/Settings";
 import DialogsConteiner from "./Component/Dialogs/DialogsConteiner";
-import UsersConteiner from "./Component/Users/UsersConteiner";
-
 import Musicc from "./Component/Redux/music";
 import HeaderContainer from "./Component/Header/HeaderContainer";
 import Login from "./Login/Login.";
-
-
-
+import Preloader from "./Component/components/common/Preloader/Preloader";
+const UsersConteiner = React.lazy(() => import('./Component/Users/UsersConteiner')   );
+const ProfileContainer = React.lazy(() => import('./Component/Profile/ProfileContainer') );
 function App(props) {
-
   return (
     <BrowserRouter>
                 <div className='app-wrapper'>
@@ -28,6 +21,7 @@ function App(props) {
 
 
               <div className='app-wrapper-content'>
+                  <Suspense fallback={<div> <Preloader /> </div>}>
                   <Routes>
                       <Route path='profile' element={<ProfileContainer/>} >
                           <Route path=':userId' element={<ProfileContainer/>} />
@@ -38,9 +32,7 @@ function App(props) {
                       <Route path="/music/" element={<Musicc/>}/>
                       <Route path="/settings" element={<Settings />}/>
                       <Route path="/login" element={<Login />}/>
-
-
-                  </Routes>
+                  </Routes> </Suspense>
               </div>
           </div>
       </BrowserRouter>
